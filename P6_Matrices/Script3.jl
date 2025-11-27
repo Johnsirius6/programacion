cf66bda220cc5ccb140bd76e0dcb2ed2df9a3a8a
John Miranda <156605785+Johnsirius6@users.noreply.github.com>
2025-11-27 17:22:54 -0500

    Create Script3.jl

---
+++ P6_Matrices/Script3.jl
@@ -0,0 +1,21 @@
+A = [1 2 3; 4 5 6; 7 8 9]
+B = [2 2 2; 2 2 2; 2 2 2]
+
+function matriz(b)
+conteo_filas = 0
+conteo_col = 0
+
+for i in b[1:end, 1:1]
+  conteo_filas = conteo_filas + 1
+end
+for j in b[1:1, 1:end]
+  conteo_col = conteo_col + 1
+end
+return conteo_filas, conteo_col
+end
+
+if matriz(A) == matriz(B)
+  return A * B
+else
+  return "No es posible operar matrices de diferentes a 3 x 3"
+end
